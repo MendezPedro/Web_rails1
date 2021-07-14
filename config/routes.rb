67d73966de2c36232956f6444a9e00fe5c5af369
Pedro <mendez.pedro94@gmail.com>
2021-07-13 21:36:50 -0400

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  # root 'home#index'
+  root 'pages#one'
+  get 'pages/one'
+  get 'pages/two'
   get 'home/index'
-  root 'home#index'
+  
   get 'contact', to: 'home#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
